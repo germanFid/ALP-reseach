2a70c62870c24a303a8f399797b15aea3c251873
germanFid <german.fidarov@gmail.com>
2024-03-20 03:38:35 +0300

    wip binrw

--- src/binrw_new.h
+++ src/binrw_new.h
@@ -1,22 +1,35 @@
-struct prefix
+#include "csv_table.h"
+
+struct Prefix
 {
     unsigned int compression_type: 1;
     unsigned int columns: 7;
 };
 
-struct core
+union PrefixWrapper
 {
-    unsigned int n_rows_block: 4;
-    unsigned int delta: 4;
-    unsigned int shift: 1;
+    struct Prefix _prefix;
+    unsigned char _val;
 };
 
-struct bin_data
+
+struct Core
 {
-    unsigned char compression_type;
-    struct prefix prefix;
-    struct core core;
+    unsigned int n_rows_block;
+    unsigned int delta;
+    unsigned char shift;
+};
+
+struct BinData
+{
+    // unsigned char compression_type;
+    union PrefixWrapper prefix;
+    struct Core core;
 
     int* values;
+    int** col_values;
     double* extras;
 };
+
+// int write_bin(struct BinData*, char*);
+int write_bin(struct BinData* data, char* filename, int n_rows, int n_cols);
